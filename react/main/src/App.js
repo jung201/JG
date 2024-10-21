f9172de0c74eb730064c06bf664e9f575022a5de
junggyu <jungkyu201@naver.com>
2024-10-21 20:49:16 +0900

    123

--- react/main/src/App.js
+++ react/main/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './clone/header'; // Header 컴포넌트 import
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
         <Router>
             <Header />
             <page1 />
+            {/* <Routes> */}
+
+            {/* 예시로 몇 가지 페이지 추가 */}
+            {/* <Route path="/" element={<HomePage />} /> */}
+            {/* <Route path="/main" element={<MainPage />} /> */}
+            {/* </Routes> */}
         </Router>
     );
 }
