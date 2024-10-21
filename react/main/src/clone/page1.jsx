0ed4ddec4906489d574a39a735ecf5ec9a5520f4
junggyu <jungkyu201@naver.com>
2024-10-21 19:59:18 +0900

    asd

---
+++ react/main/src/clone/page1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./page1.module.scss";
+import { Link } from "react-router-dom";
+
+function Page1() {
+  return decodeURI;
+}
+
+export default Header;
