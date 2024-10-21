9d01730aeb126f2bda779bac1267d5688da2ad3a
junggyu <jungkyu201@naver.com>
2024-10-21 20:36:18 +0900

    asd

--- react/main/src/App.js
+++ react/main/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Header from './clone/header'; // Header 컴포넌트 import
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
     return (
         <Router>
             <Header />
-            <Routes>
-                {/* 예시로 몇 가지 페이지 추가 */}
-                {/* <Route path="/" element={<HomePage />} /> */}
-                {/* <Route path="/main" element={<MainPage />} /> */}
-            </Routes>
+            <page1 />
         </Router>
     );
 }
--- react/main/src/clone/page1.jsx
+++ react/main/src/clone/page1.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import styles from "./page1.module.scss";
-import { Link } from "react-router-dom";
 
-function Page1() {
-  return decodeURI;
-}
+import photo38 from './img/사진38.jpeg';
+import photo34 from './img/사진34.jpeg';
+import photo32 from './img/사진32.jpeg';
+import photo33 from './img/사진33.jpeg';
+import photo40 from './img/사진40.jpg';
 
-export default Header;
+const MainBox = () => {
+  return (
+    <div className="mainBox">
+      {/* 왼쪽 사진 부분 */}
+      <div className="left">
+        {/* 메인 슬라이더 */}
+        <section>
+          <div className="pic_container">
+            <ul className="pic_slider">
+              <li><img src={photo38} alt="img23" /></li>
+              <li><img src={photo34} alt="img29" /></li>
+              <li><img src={photo32} alt="img25" /></li>
+              <li><img src={photo33} alt="img25" /></li>
+              <li><img src={photo40} alt="img25" /></li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default MainBox;
