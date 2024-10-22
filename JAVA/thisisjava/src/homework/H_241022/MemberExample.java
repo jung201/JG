b3a84927225e4cba52335035f7b1449216dc5d4e
junggyu <jungkyu201@naver.com>
2024-10-22 19:20:12 +0900

    12

---
+++ JAVA/thisisjava/src/homework/H_241022/MemberExample.java
@@ -0,0 +1,12 @@
+package homework.H_241022;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+
+		Member user1 = new Member("홍길동", "hong");
+
+		System.out.println(user1.name);
+		System.out.println(user1.id);
+	}
+}
