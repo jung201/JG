fb9a4c806ad92fe93ec904e4a3c4f9226b253e3f
junggyu <jungkyu201@naver.com>
2024-10-22 20:31:36 +0900

    12

--- JAVA/thisisjava/src/homework/H_241022/Member.java
+++ JAVA/thisisjava/src/homework/H_241022/Member.java
@@ -1,14 +1,15 @@
 package homework.H_241022;
 
 public class Member {
-	String name;
-	String id;
-	String password;
-	int age;
 
-	Member(String name, String id) {
-		this.name = name;
-		this.id = id;
-	}
+    String name;
+    String id;
+    String password;
+    int age;
+
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 
 }
