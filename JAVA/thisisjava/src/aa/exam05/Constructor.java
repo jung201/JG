ea09d9b44167f91bd08ad8dcda9187769e8391fb
junggyu <jungkyu201@naver.com>
2024-10-28 22:36:22 +0900

    121212121212121212

--- JAVA/thisisjava/src/aa/exam05/Constructor.java
+++ JAVA/thisisjava/src/aa/exam05/Constructor.java
@@ -8,6 +8,9 @@ class A {
 	void work() {
 		System.out.println(m); // 0
 	}
+//	A(){ // 자동으로 추가된 기본 생성자 
+//		
+//	}
 }
 
 class B {
@@ -18,14 +21,13 @@ class B {
 	}
 
 	B() { // 기본생성자
-
 	}
 }
 
 class C {
 	int m;
 
-	void work() { 
+	void work() {
 		System.out.println(m); // 생성자로 넘어온 값
 	}
 
@@ -36,16 +38,16 @@ class C {
 
 public class Constructor {
 	public static void main(String[] args) {
-		
+
 		// 1. 클래스의 객체 생성
 		A a = new A();
 		B b = new B();
 		C c = new C(3);
-		
+
 		a.work();
 		b.work();
 		c.work();
-		
+
 	}
 
 }
--- JAVA/thisisjava/src/aa/exam05/Overloading.java
+++ JAVA/thisisjava/src/aa/exam05/Overloading.java
@@ -19,6 +19,8 @@ public class Overloading {
 		AA a1 = new AA(); // 첫번째 생성자
 		AA a2 = new AA(3); // 두번째 생성자
 		AA a3 = new AA(3,5); // 세번째 생성자
+		
+		// 생성자의 숫자는 출력하거나 사용하지않으면 의미가 없다 3이든 5든 100이든
 	}
 
 }
